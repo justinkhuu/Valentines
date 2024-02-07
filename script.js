80fa695b7933c8aa005897f7ba80aec025be5c09
Justin Khuu <justin.khuu2@gmail.com>
2024-02-07 01:26:55 -0500

    2nd set of cards and images added

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-function showCards() {
+let firstCardSelected = false;
+let selectedFirstCardId;
+let firstCardMessage;
+
+function showFirstCards() {
     document.querySelector('.buttons').style.display = 'none';
-    document.getElementById('cards').style.display = 'block';
+    document.getElementById('firstCards').style.display = 'block';
+}
+
+function selectFirstCard(cardId) {
+    firstCardSelected = true;
+    selectedFirstCardId = cardId;
+    firstCardMessage = document.getElementById(`firstCard${cardId}Message`).textContent;
+
+    document.getElementById('firstCards').style.display = 'none';
+    document.getElementById('secondCards').style.display = 'block';
+}
+
+function showFinalCard(chosenMessage, chosenImage) {
+    let finalMessage;
+
+    if (firstCardSelected) {
+        document.getElementById('secondCards').style.display = 'none';
+        document.getElementById('finalCard').style.display = 'block';
+
+        finalMessage = firstCardMessage + '<br>' + chosenMessage;
+
+        document.getElementById('finalMessage').innerHTML = finalMessage;
+        document.getElementById('finalImage1').src = `image${selectedFirstCardId}.jpg`;
+        document.getElementById('finalImage2').src = chosenImage;
+    }
 }
 
 function showHeartbreak() {
@@ -10,6 +38,20 @@ function showHeartbreak() {
 
 function goBack() {
     document.querySelector('.buttons').style.display = 'block';
-    document.getElementById('cards').style.display = 'none';
+    document.getElementById('firstCards').style.display = 'none';
+    document.getElementById('secondCards').style.display = 'none';
     document.getElementById('heartbreak').style.display = 'none';
+    document.getElementById('finalCard').style.display = 'none';
+}
+
+function restart() {
+    document.querySelector('.buttons').style.display = 'block';
+    document.getElementById('firstCards').style.display = 'none';
+    document.getElementById('secondCards').style.display = 'none';
+    document.getElementById('heartbreak').style.display = 'none';
+    document.getElementById('finalCard').style.display = 'none';
+
+    firstCardSelected = false;
+    selectedFirstCardId = undefined;
+    firstCardMessage = undefined;
 }
